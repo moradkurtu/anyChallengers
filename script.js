46ed28031d25e46685a0e2efc02e5661af9065a7
Morad Kurtu <moradkurtu@yahoo.com>
2022-04-19 21:33:56 -0700

    added js code

---
+++ script.js
@@ -0,0 +1,17 @@
+const startButton = document.getElementById("start-btn")
+const questionContainerElement = document.getElementById("question-container")
+startButton.addEventListener("click", startGame);
+
+
+function startGame(){
+    console.log('Started')
+    startButton.classList.add("hide")
+}
+function setNextGame(){
+
+}
+
+function selectAnswer(){
+
+}
+
